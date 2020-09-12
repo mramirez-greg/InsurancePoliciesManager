d2cd089980fcfb721dfbcc6bcb2e784660a03df5
Mauricio Ramirez <mauricio.ramirez@outlook.com>
2020-09-12 09:15:40 -0500

    add vs projects to solution

---
+++ InsurancePoliciesManager/Gap.IPM.ApplicationUnitTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Gap.IPM.ApplicationUnitTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
