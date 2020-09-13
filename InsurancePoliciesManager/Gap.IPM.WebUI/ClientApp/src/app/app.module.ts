3f81f70e22078e919a0e5751abd37caa3bf1f1b7
Mauricio Ramirez <mauricio.ramirez@outlook.com>
2020-09-13 10:59:58 -0500

    simple  html demo for inspiring me

--- InsurancePoliciesManager/Gap.IPM.WebUI/ClientApp/src/app/app.module.ts
+++ InsurancePoliciesManager/Gap.IPM.WebUI/ClientApp/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
@@ -14,6 +15,8 @@ import { AuthorizeGuard } from 'src/api-authorization/authorize.guard';
 import { AuthorizeInterceptor } from 'src/api-authorization/authorize.interceptor';
 import { PoliciesComponent } from './policies/policies.component';
 import { CustomersComponent } from './customers/customers.component';
+import { ModalModule } from 'ngx-bootstrap/modal';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -27,14 +30,17 @@ import { CustomersComponent } from './customers/customers.component';
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
+    FontAwesomeModule,
     HttpClientModule,
     FormsModule,
     ApiAuthorizationModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent, canActivate: [AuthorizeGuard] },
-    ])
+      { path: 'customers', component: CustomersComponent, canActivate: [AuthorizeGuard]  },
+      { path: 'policies', component: PoliciesComponent},
+    ]),
+    BrowserAnimationsModule,
+    ModalModule.forRoot()
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: AuthorizeInterceptor, multi: true }
--- InsurancePoliciesManager/Gap.IPM.WebUI/ClientApp/src/app/policies/policies.component.ts
+++ InsurancePoliciesManager/Gap.IPM.WebUI/ClientApp/src/app/policies/policies.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
+import { faPlus, faEllipsisH } from '@fortawesome/free-solid-svg-icons';
+import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
 
 @Component({
   selector: 'app-policies',
@@ -7,9 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PoliciesComponent implements OnInit {
 
-  constructor() { }
+  faPlus = faPlus;
+  faEllipsisH = faEllipsisH;
+  policyDetailsModalRef: BsModalRef;
+  constructor(private modalService: BsModalService) { }
 
   ngOnInit() {
+  }
+
+  showPolicyDetailsModal(template: TemplateRef<any>): void {
+   
+
+    this.policyDetailsModalRef = this.modalService.show(template);
   }
 
 }
